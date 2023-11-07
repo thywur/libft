e0a03c2e97e4f68a6b86084e08126db9a4897b2b
Aleksandr Ermolov <alermolo@z4r6p1.42lyon.fr>
2023-11-07 17:19:27 +0100

    mod isalnum memcpy

--- srcs/ft_isalnum.c
+++ srcs/ft_isalnum.c
@@ -6,23 +6,11 @@
 /*   By: alermolo <alermolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 15:31:29 by alermolo          #+#    #+#             */
-/*   Updated: 2023/11/06 15:36:10 by alermolo         ###   ########.fr       */
+/*   Updated: 2023/11/07 16:54:16 by alermolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isalpha(int c)
-{
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	return (0);
-}
-
-int	ft_isdigit(int c)
-{
-	if (c >= '0' && c <= '9')
-		return (1);
-	return (0);
-}
+#include "libft.h"
 
 int	ft_isalnum(int c)
 {
--- srcs/ft_memcpy.c
+++ srcs/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: alermolo <alermolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 16:46:50 by alermolo          #+#    #+#             */
-/*   Updated: 2023/11/06 16:55:28 by alermolo         ###   ########.fr       */
+/*   Updated: 2023/11/07 16:32:44 by alermolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		((unsigned char *)dest)[i] = ((unsigned char*)src)[i];
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
 		i++;
 	}
 	return (dest);
