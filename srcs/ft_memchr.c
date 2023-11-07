843c60ade3a124ce1b6311308670b49d713e402b
Aleksandr Ermolov <alermolo@z4r6p1.42lyon.fr>
2023-11-07 18:04:13 +0100

    memchr

---
+++ srcs/ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alermolo <alermolo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 17:52:57 by alermolo          #+#    #+#             */
+/*   Updated: 2023/11/07 18:03:50 by alermolo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*str;
+	unsigned long	i;
+
+	str = (unsigned char *)s;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (&str[i]);
+		i++;
+	}
+	return (NULL);
+}
