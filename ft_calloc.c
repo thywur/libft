ce63e33b40438fdd26a3095083feb719dc409a3b
Aleksandr Ermolov <alermolo@z3r8p1.42lyon.fr>
2023-11-10 12:44:12 +0100

    calloc test

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,24 +6,24 @@
 /*   By: alermolo <alermolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 19:08:08 by alermolo          #+#    #+#             */
-/*   Updated: 2023/11/08 18:08:13 by alermolo         ###   ########.fr       */
+/*   Updated: 2023/11/10 12:43:39 by alermolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t nmemb, size_t size)
-{
-	void	*arr;
+// void	*ft_calloc(size_t nmemb, size_t size)
+// {
+// 	void	*arr;
 
-	if (nmemb * size > INT_MAX)
-		return (NULL);
-	arr = malloc(nmemb * size);
-	// if (!arr || nmemb == 0 || size == 0)
-	// 	return (NULL);
-	ft_bzero(arr, nmemb);
-	return (arr);
-}
+// 	if (nmemb * size > INT_MAX)
+// 		return (NULL);
+// 	arr = malloc(nmemb * size);
+// 	// if (!arr || nmemb == 0 || size == 0)
+// 	// 	return (NULL);
+// 	ft_bzero(arr, nmemb);
+// 	return (arr);
+// }
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
