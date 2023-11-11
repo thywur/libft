66be7fb0b37983aa77104f405a1a932788ab1017
Aleksandr Ermolov <alermolo@z3r10p3.42lyon.fr>
2023-11-11 17:31:59 +0100

    lstmap fix

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: alermolo <alermolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 16:49:19 by alermolo          #+#    #+#             */
-/*   Updated: 2023/11/10 17:08:12 by alermolo         ###   ########.fr       */
+/*   Updated: 2023/11/11 17:31:42 by alermolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new;
 
 	if (!lst || !f || !del)
-		return ;
+		return (NULL);
 	new = ft_lstnew((*f)(lst->content));
 	ft_lstdelone(lst, del);
 	while (lst)
