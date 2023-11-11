05098bf04d2a854961653fda39cf84216f4401f5
Aleksandr Ermolov <alermolo@z3r10p3.42lyon.fr>
2023-11-11 18:20:35 +0100

    lstclear lstmap

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,16 +6,29 @@
 /*   By: alermolo <alermolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 16:31:43 by alermolo          #+#    #+#             */
-/*   Updated: 2023/11/10 16:43:53 by alermolo         ###   ########.fr       */
+/*   Updated: 2023/11/11 18:20:05 by alermolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// void	ft_lstclear(t_list **lst, void (*del)(void *))
+// {
+// 	if (!lst || !del)
+// 		return ;
+// 	while (lst)
+// 	{
+// 		ft_lstdelone(*lst, del);
+// 		*lst = *lst->next;
+// 	}
+// 	lst = NULL;
+// }
+
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	if (!del)
+	if (!lst || !del)
 		return ;
-	while (lst)
-		ft_lstdelone(*lst, del);
+	ft_lstclear(&(*lst)->next, del);
+	ft_lstdelone(*lst, del);
+	*lst = NULL;
 }
--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,24 +6,48 @@
 /*   By: alermolo <alermolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 16:49:19 by alermolo          #+#    #+#             */
-/*   Updated: 2023/11/11 17:31:42 by alermolo         ###   ########.fr       */
+/*   Updated: 2023/11/11 18:09:49 by alermolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+// {
+// 	t_list	*new;
+
+// 	if (!lst || !f || !del)
+// 		return (NULL);
+// 	new = ft_lstnew((*f)(lst->content));
+// 	if (!new)
+// 		return (NULL);
+// 	ft_lstdelone(lst, del);
+// 	while (lst)
+// 	{
+// 		ft_lstadd_back(&new, ft_lstnew((*f)(lst->content)));
+// 		ft_lstdelone(lst, del);
+// 	}
+// 	return (new);
+// }
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
+	t_list	*map;
 
-	if (!lst || !f || !del)
+	if (!f || !del)
 		return (NULL);
-	new = ft_lstnew((*f)(lst->content));
-	ft_lstdelone(lst, del);
+	map = NULL;
 	while (lst)
 	{
-		ft_lstadd_back(&new, ft_lstnew((*f)(lst->content)));
-		ft_lstdelone(lst, del);
+		new = ft_lstnew((*f)(lst->content));
+		if (!new)
+		{
+			ft_lstclear(&map, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&map, new);
+		lst = lst->next;
 	}
-	return (new);
+	return (map);
 }
