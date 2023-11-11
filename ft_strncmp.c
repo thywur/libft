0fc9d83983bf3f5db996be749d6b60522ae6b6af
Aleksandr Ermolov <alermolo@z3r10p3.42lyon.fr>
2023-11-11 15:38:43 +0100

    changed strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,24 +6,38 @@
 /*   By: alermolo <alermolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 17:40:17 by alermolo          #+#    #+#             */
-/*   Updated: 2023/11/07 17:47:00 by alermolo         ###   ########.fr       */
+/*   Updated: 2023/11/11 15:38:00 by alermolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+// int	ft_strncmp(const char *s1, const char *s2, size_t n)
+// {
+// 	unsigned long	i;
+
+// 	i = 0;
+// 	while (s1[i] && (i < n))
+// 	{
+// 		if (s1[i] != s2[i])
+// 			return (s1[i] - s2[i]);
+// 		i++;
+// 	}
+// 	if (i == n)
+// 		return (0);
+// 	return (s1[i] - s2[i]);
+// }
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	unsigned long	i;
 
 	i = 0;
-	while (s1[i] && (i < n))
+	while ((s1[i] || s2[i]) && i < n)
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
 		i++;
 	}
-	if (i == n)
-		return (0);
-	return (s1[i] - s2[i]);
+	return (0);
 }
