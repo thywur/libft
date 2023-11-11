5c4052f34d990f1368430259d8e80a53dab12276
Aleksandr Ermolov <alermolo@z3r10p3.42lyon.fr>
2023-11-11 15:59:02 +0100

    fix strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: alermolo <alermolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 17:40:17 by alermolo          #+#    #+#             */
-/*   Updated: 2023/11/11 15:38:00 by alermolo         ###   ########.fr       */
+/*   Updated: 2023/11/11 15:58:18 by alermolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while ((s1[i] || s2[i]) && i < n)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	return (0);
