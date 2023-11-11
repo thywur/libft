092d529c43e1c0bc3910e65a95a22f3777547213
Aleksandr Ermolov <alermolo@z3r10p3.42lyon.fr>
2023-11-11 15:26:22 +0100

    changed split strlcat

--- ft_split.c
+++ ft_split.c
@@ -6,78 +6,137 @@
 /*   By: alermolo <alermolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 12:24:01 by alermolo          #+#    #+#             */
-/*   Updated: 2023/11/10 15:44:48 by alermolo         ###   ########.fr       */
+/*   Updated: 2023/11/11 15:18:58 by alermolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ft_strndup(const char *s, int n)
+static char	*ft_strndup(char *src, int n)
 {
 	int		i;
-	char	*dest;
+	char	*dup;
 
 	i = 0;
-	dest = (char *)malloc((n + 1) * sizeof(char));
-	if (!dest)
+	dup = malloc(sizeof(char) * (n + 1));
+	if (!dup)
 		return (NULL);
-	while (i < n)
+	while (src[i] && i < n)
 	{
-		dest[i] = s[i];
+		dup[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
-	return (dest);
+	dup[i] = '\0';
+	return (dup);
 }
 
-static int	get_split_size(char const *s, char c)
+// static int	get_split_size(char const *s, char c)
+// {
+// 	int	i;
+// 	int	size;
+
+// 	i = 0;
+// 	size = 0;
+// 	while (s[i] == c)
+// 		i++;
+// 	if (s[i] != c && s[i] != '\0')
+// 		size++;
+// 	while (s[i])
+// 	{
+// 		if (s[i + 1] != c && s[i + 1] != '\0' && s[i] == c)
+// 		{
+// 			size++;
+// 			while (s[i + 1] && s[i] == c)
+// 				i++;
+// 		}
+// 		i++;
+// 	}
+// 	return (size);
+// }
+
+static int	ft_get_split_size(char const *str, char sep)
 {
 	int	i;
 	int	size;
 
 	i = 0;
 	size = 0;
-	while (s[i] == c)
-		i++;
-	if (s[i] != c && s[i] != '\0')
-		size++;
-	while (s[i])
+	while (str[i])
 	{
-		if (s[i + 1] != c && s[i + 1] != '\0' && s[i] == c)
-		{
+		if (str[i] != sep && str[i + 1] == sep || str[i + 1] == '\0')
 			size++;
-			while (s[i + 1] && s[i] == c)
-				i++;
-		}
 		i++;
 	}
 	return (size);
 }
 
-char	**ft_split(char const *s, char c)
-{
-	int		i;
-	int		j;
-	int		save;
-	int		size;
-	char	**split;
+// char	**ft_split(char const *s, char c)
+// {
+// 	int		i;
+// 	int		j;
+// 	int		save;
+// 	int		size;
+// 	char	**split;
 
-	size = get_split_size(s, c);
-	split = (char **)malloc(sizeof(char *) * (size + 1));
-	if (!split)
-		return (NULL);
+// 	size = get_split_size(s, c);
+// 	split = (char **)malloc(sizeof(char *) * (size + 1));
+// 	if (!split)
+// 		return (NULL);
+// 	i = 0;
+// 	j = 0;
+// 	while (s[i] && j < size)
+// 	{
+// 		while (s[i] == c)
+// 			i++;
+// 		save = i;
+// 		while (s[i] && s[i] != c)
+// 			i++;
+// 		split[j++] = ft_strndup(&s[save], i - save);
+// 		i++;
+// 	}
+// 	split[j] = 0;
+// 	return (split);
+// }
+
+static void	ft_fill_split(char **split, char *str, char sep, int split_size)
+{
+	int	save;
+	int	i;
+	int	line_no;
+
 	i = 0;
-	j = 0;
-	while (s[i] && j < size)
+	line_no = 0;
+	while (str[i] && str[i] == sep)
+		i++;
+	save = i;
+	while (str[i] && line_no < split_size)
 	{
-		while (s[i] == c)
-			i++;
-		save = i;
-		while (s[i] && s[i] != c)
-			i++;
-		split[j++] = ft_strndup(&s[save], i - save);
+		if (i != 0 && str[i - 1] != sep && str[i] == sep)
+		{
+			split[line_no] = ft_strndup(&str[save], i - save);
+			line_no++;
+		}
+		if (i != 0 && str[i - 1] == sep && str[i] != sep)
+			save = i;
 		i++;
 	}
-	split[j] = 0;
+	if (i != 0 && str[i] == '\0' && str[i - 1] != sep)
+		split[line_no] = ft_strndup(&str[save], i);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**split;
+	int		split_size;
+
+	split_size = ft_get_split_size(s, c);
+	split = (char **)malloc(sizeof(char *) * split_size + 1);
+	if (!split)
+		return (NULL);
+	ft_fill_split(split, s, c, split_size);
+	split[split_size] = (char *)(malloc(sizeof(char)));
+	if (!split[split_size])
+		return (NULL);
+	split[split_size] = 0;
 	return (split);
 }
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,30 +6,70 @@
 /*   By: alermolo <alermolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 16:55:06 by alermolo          #+#    #+#             */
-/*   Updated: 2023/11/07 17:17:12 by alermolo         ###   ########.fr       */
+/*   Updated: 2023/11/11 15:25:43 by alermolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
+// size_t	ft_strlcat(char *dst, const char *src, size_t size)
+// {
+// 	unsigned long	i;
+// 	unsigned long	dst_size;
+// 	unsigned long	src_size;
+
+// 	dst_size = ft_strlen(dst);
+// 	src_size = ft_strlen(src);
+// 	i = 0;
+// 	if (size < dst_size)
+// 		src_size += size;
+// 	else
+// 		src_size += dst_size;
+// 	while (src[i] && dst_size + i < size - 1 && size != '\0')
+// 	{
+// 		dst[dst_size + i] = src[i];
+// 		i++;
+// 	}
+// 	dst[dst_size + i] = '\0';
+// 	return (src_size);
+// }
+
+static char	*ft_strncat(char *dest, char *src, size_t nb)
 {
 	unsigned long	i;
-	unsigned long	dst_size;
+	unsigned long	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i])
+		i++;
+	while (src[j] && j < nb)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	unsigned long	dest_size;
 	unsigned long	src_size;
 
-	dst_size = ft_strlen(dst);
+	dest_size = ft_strlen(dst);
 	src_size = ft_strlen(src);
-	i = 0;
-	if (size < dst_size)
-		src_size += size;
-	else
-		src_size += dst_size;
-	while (src[i] && dst_size + i < size - 1 && size != '\0')
+	if (size <= dest_size)
+		return (size + src_size);
+	else if (size <= dest_size + src_size)
 	{
-		dst[dst_size + i] = src[i];
-		i++;
+		ft_strncat(dst, src, size - dest_size - 1);
+		return (dest_size + src_size);
 	}
-	dst[dst_size + i] = '\0';
-	return (src_size);
+	else
+	{
+		ft_strncat(dst, src, src_size);
+		return (dest_size + src_size);
+	}
 }
